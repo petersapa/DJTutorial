49aa47f949731e1ca853f3e5986b846348848594
petersapa <psanchez@sapalomera.cat>
2025-05-18 18:41:08 +0200

    final: vistes admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from .models import *
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
 admin.site.register(Question)
 admin.site.register(Choice)
